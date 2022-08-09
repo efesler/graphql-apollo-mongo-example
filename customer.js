2479c85e67b5b2aa578f6ee28d9bb0503fdbc0db
eric <eric.fesler@contraste.com>
2022-08-09 07:31:59 +0000

    use graphql-compose-mongoose to generate the resolvers. Add filters and pagination as well

--- customer.js
+++ customer.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { composeMongoose } = require("graphql-compose-mongoose");
 
 // Define schema
 // https://mongoosejs.com/docs/models.html#compiling
@@ -6,24 +7,28 @@ const AddressSchema = mongoose.Schema({
     city: String,
     street: String,
     houseNumber: String,
-    });
+});
     
-    const ContactInfoSchema = mongoose.Schema({
+const ContactInfoSchema = mongoose.Schema({
     tel: [String],
     email: [String],
     address: {
         type: AddressSchema,
         required: true,
     },
-    });
-    
-    const CustomerSchema = mongoose.Schema({
+});
+
+const CustomerSchema = mongoose.Schema({
     firstName: String,
     lastName: String,
     company: String,
     connectInfo: ContactInfoSchema,
-    });
-    
-    const CustomerModel = mongoose.model("Customer", CustomerSchema);
+});
 
-    module.exports = { CustomerModel };
+const CustomerModel = mongoose.model("Customer", CustomerSchema);
+
+// CONVERT MONGOOSE MODEL TO GraphQL PIECES
+const customizationOptions = {}; // left it empty for simplicity
+const CustomerTC = composeMongoose(CustomerModel, customizationOptions);
+
+module.exports = { CustomerTC };
--- index.js
+++ index.js
@@ -1,61 +1,19 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { CustomerModel } = require('./customer');
+const {CustomerTC } = require('./customer');
 const connectDB = require("./db");
+const { schemaComposer } = require('graphql-compose');
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+schemaComposer.Query.addFields({
+    customers: CustomerTC.mongooseResolvers.findMany({ lean: true }),  
+    customer: CustomerTC.mongooseResolvers.findOne({ lean: true }),
+    customerPagination: CustomerTC.mongooseResolvers.pagination({ lean: true }),
+    customerCount: CustomerTC.mongooseResolvers.count({ lean: true }),
+});
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Customer {
-    firstName: String,
-    lastName: String,
-    company: String,
-    connectInfo: ContactInfoSchema
-  }
+// Build graphql schema
+const schema = schemaComposer.buildSchema();
 
-  type ContactInfoSchema {
-    tel: [String],
-    email: [String],
-    address: AddressSchema
-  }
-
-  type AddressSchema {
-    city: String,
-    street: String,
-    houseNumber: String
-  }
-
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    customers: [Customer]
-    customer(firstName: String): Customer
-  }
-
-`;
-
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-        customers:  () => {
-          return new Promise((resolve,reject)=>{
-            CustomerModel.find((err,customers)=>{
-                if(err) reject(err);
-                else resolve(customers);
-            })
-          })
-    
-        }
-    },
-};
-
+// Create an apollo server
 const {
   ApolloServerPluginLandingPageLocalDefault
 } = require('apollo-server-core');
@@ -63,12 +21,9 @@ const {
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
-  typeDefs,
-  // introspection: true,
-  // mocks: true, // TODO: Remove in PROD.
-  // mockEntireSchema: false, // TODO: Remove in PROD.
-  // playground: true,
-  resolvers,
+  schema: schema,
+  introspection: true,
+  
   csrfPrevention: true,
   cache: 'bounded',
   /**
